e0b0a2b68d15d9ded8b3194e835041622b1b1b98
nuraiamanzholova <amanzholova@publicplan.de>
2023-09-27 10:33:45 +0200

    mit opacity

--- modelfenster.js
+++ modelfenster.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-let logo = document.getElementById("logoo");
+let logo = document.querySelector(".logo");
 let close = document.getElementById('close');
 let modal = document.getElementById('modal');
 
 
 logo.addEventListener('click', function () {
     modal.style.display = "block";
-    modal.style.opacity ="1";
+
 });
 
 close.addEventListener('click', function() {
