cf26d14b5a64ce67c0fc1b9e9bafdbb4c04ca62c
nuraiamanzholova <amanzholova@publicplan.de>
2023-09-29 14:38:51 +0200

    allws

--- modelfenster.js
+++ modelfenster.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
 let logo = document.querySelector(".logo");
 let close = document.getElementById('close');
 let modal = document.getElementById('modal');
-
+let background =document.getElementById('background');
 
 logo.addEventListener('click', function () {
     modal.style.display = "block";
@@ -11,6 +11,11 @@ logo.addEventListener('click', function () {
 
 close.addEventListener('click', function() {
     modal.style.display = "none";
+    background.style.display = 'none';
 })
+logo.addEventListener('click', function(){
+background.style.display = 'block';
+})
+
 
 });
